19b73baed9cad9f71b85a30e69c451f7ebe93bf3
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2024-12-03 17:05:12 -0300

    Ayudantia: Buscador

--- src/app/_shared/page/products/products.component.ts
+++ src/app/_shared/page/products/products.component.ts
@@ -2,29 +2,43 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../products.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ola-products',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
-  export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit {
+  productsService = inject(ProductsService);
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  searchQuery: string = '';
 
+  constructor() {}
 
-    productsService = inject(ProductsService);
-    products: any = [];
+  ngOnInit(): void {
+    this.productsService
+      .getProducts()
+      .then((data) => {
+          this.products = data.data;
+          this.filteredProducts = this.products;
+        })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
 
-    constructor(){}
-
-    ngOnInit(): void {
-      this.productsService.getProducts().then((data: any) => {
-        this.products = data.data;
-      }
-      //Validar que la lista este vacia o haya algun error
+  filterProducts(): void {
+    const query = this.searchQuery.toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      Object.values(product)
+        .join(' ')
+        .toLowerCase()
+        .includes(query)
     );
-    }
-
+  }
 }
